e2eef7f10db7303d309e093522c0e6420fb9f520
ripa01 <ripabiswas2739@gmail.com>
2024-08-10 01:02:10 +0600

    add some necessary image files

---
+++ Job Board/frontend/src/assets/index.js
@@ -0,0 +1,22 @@
+import NoProfile from "./user.png";
+import Google from "./google.png";
+import Linkedin from "./linkedin.png";
+import Youtube from "./youtube.png";
+import WhatsApp from "./whatsapp.png";
+import Twitter from "./twitter.png";
+import Instagram from "./instagram.png";
+import Spotify from "./spotify.png";
+import Facebook from "./facebook.png";
+
+
+export {
+  NoProfile,
+  Facebook,
+  WhatsApp,
+  Twitter,
+  Instagram,
+  Spotify,
+  Google,
+  Linkedin,
+  Youtube,
+};
