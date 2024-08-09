c879270ba8ed133e4a15ecf7835b20027b32c461
ripa01 <ripabiswas2739@gmail.com>
2024-08-10 00:54:55 +0600

    install & setup tailwind css

--- Job Board/frontend/src/App.jsx
+++ Job Board/frontend/src/App.jsx
@@ -5,7 +5,9 @@ function App() {
     <>
       <div>
    
-      <h1>Vite + React</h1>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
       
       </div>
     
