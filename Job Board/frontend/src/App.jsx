06595d389f2c735bfa24debbfa038fd44bc3ad33
ripa01 <ripabiswas2739@gmail.com>
2024-08-10 12:12:58 +0600

    App. component is designed to handle routing and layout using react-router-dom.

--- Job Board/frontend/src/App.jsx
+++ Job Board/frontend/src/App.jsx
@@ -1,15 +1,53 @@
+import { Outlet, Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { Footer, Navbar } from "./components";
+
+function Layout() {
+  const { user } = true;
+  const location = useLocation();
+
+  return user ? (
+    <Outlet />
+  ) : (
+    <Navigate to='/user-auth' state={{ from: location }} replace />
+  );
+}
+
 function App() {
 
 
   return (
     <>
-      <div>
-   
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-      
-      </div>
+      <main className='bg-[#f7fdfd]'>
+      <Navbar />
+
+      <Routes>
+        <Route element={<Layout />}>
+          <Route
+            path='/'
+            element={<Navigate to='/find-jobs' replace={true} />}
+          />
+          <Route path='/find-jobs' element={<FindJobs />} />
+          <Route path='/companies' element={<Companies />} />
+          <Route
+            path={
+              user?.user?.accountType === "seeker"
+                ? "/user-profile"
+                : "/user-profile/:id"
+            }
+            element={<UserProfile />}
+          />
+
+          <Route path={"/company-profile"} element={<CompanyProfile />} />
+          <Route path={"/company-profile/:id"} element={<CompanyProfile />} />
+          <Route path={"/upload-job"} element={<UploadJob />} />
+          <Route path={"/job-detail/:id"} element={<JobDetail />} />
+        </Route>
+
+        <Route path='/about-us' element={<About />} />
+        <Route path='/user-auth' element={<AuthPage />} />
+      </Routes>
+      {user && <Footer />}
+    </main>
     
     </>
   )
