111dfbaab8e14c94a2b5c6588a99e7fcaca6b6d8
ripa01 <ripabiswas2739@gmail.com>
2024-08-13 14:02:44 +0600

    setup mongodb connection

---
+++ Job Board/backend/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
+
+import dbConnection from "./dbConfig/dbConnection.js";
+
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 8800;
+
+// MONGODB CONNECTION
+dbConnection();
+
+
+// middlenames
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan("dev"));
+
+app.listen(PORT, () => {
+  console.log(`Dev Server running on port: ${PORT}`);
+});
