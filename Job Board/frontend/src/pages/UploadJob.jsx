7a4b606350b0a29f2dd4144a8a439c23f22bb9eb
ripa01 <ripabiswas2739@gmail.com>
2024-08-19 23:11:00 +0600

    upload job post integration with backend

--- Job Board/frontend/src/pages/UploadJob.jsx
+++ Job Board/frontend/src/pages/UploadJob.jsx
@@ -2,8 +2,11 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { CustomButton, JobCard, JobTypes, TextInput } from "../components";
 import { jobs } from "../utils/data";
+import {  useSelector } from "react-redux";
+import { apiRequest } from "../utils";
 
 const UploadJob = () => {
+  const { user } = useSelector((state) => state.user);
   const {
     register,
     handleSubmit,
@@ -16,9 +19,45 @@ const UploadJob = () => {
   });
 
   const [errMsg, setErrMsg] = useState("");
-  const [jobTitle, setJobTitle] = useState("Full-Time");
+  const [jobType, setJobType] = useState("Full-Time");
+  const [isLoading, setIsLoading] = useState(false);
+  const [recentPost, setrecentPost] = useState(false);
 
-  const onSubmit = async (data) => {};
+  const onSubmit = async (data) => {
+    setIsLoading(true);
+    setErrMsg(null);
+
+    const newData = { ...data, jobType: jobType};
+    try{
+      const res = await apiRequest({
+        url : "/jobs/upload-job",
+        token: user?.token,
+        data : newData,
+        method : "POST",
+      });
+
+      
+      console.log(res);
+
+      if(res.status === "failed"){
+        setErrMsg({ ...res });
+      }else {
+        setErrMsg({ status : "success", message : res.message});
+        
+        setTimeout(() => {
+          window.location.reload();
+        },2000);
+      }
+      setIsLoading(false);
+
+    }catch(error){
+      console.log(error);
+      setIsLoading(false);
+
+    }
+
+
+  };
 
   return (
     <div className='container mt-10 mx-auto flex justify-center md:flex-row gap-8 2xl:gap-14 bg-indigo-100 px-5'>
@@ -45,7 +84,7 @@ const UploadJob = () => {
             <div className='w-full flex gap-4'>
               <div className={`w-1/2 mt-2`}>
                 <label className='text-gray-600 text-sm mb-1'>Job Type</label>
-                <JobTypes jobTitle={jobTitle} setJobTitle={setJobTitle} />
+                <JobTypes jobTitle={jobType} setJobTitle={setJobType} />
               </div>
 
               <div className='w-1/2'>
@@ -118,6 +157,17 @@ const UploadJob = () => {
                   {errors.desc?.message}
                 </span>
               )}
+            </div>
+            <div className='flex flex-col'>
+              <label className='text-gray-600 text-sm mb-1'>
+                Requirements
+              </label>
+              <textarea
+                className='rounded border border-gray-400 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 text-base px-4 py-2 resize-none'
+                rows={4}
+                cols={6}
+                {...register("requirements")}
+              ></textarea>
             </div>
 
             {errMsg && (
