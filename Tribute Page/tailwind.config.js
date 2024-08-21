f334aaed8f17600ced2f8dff0b466a59e7fe27d5
ripa01 <ripabiswas2739@gmail.com>
2024-08-21 20:48:04 +0600

    design header and main section

--- Tribute Page/tailwind.config.js
+++ Tribute Page/tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{html,js}', './src/input.css'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Open Sans', 'sans-serif'],
+      inter: ['Inter', 'sans-serif'],
+      montserrat: ['Montserrat', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        primary: "#0872BF",
+        background:"#141A1A"
+      }, 
+      dropShadow: {
+        '3xl': '0 4px 20px rgba(93, 173, 236, 0.23)',
+      }
+    },
   },
   plugins: [],
 }
